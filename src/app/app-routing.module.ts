f4dfaf62b5ad1b8aee9f6a7dcda55c1c45ea1a85
isaias9715 <king.isaias64@gmail.com>
2022-05-25 16:15:28 -0500

    log in basico para los 2 primeros menus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BatmanGuard } from './batman.guard';
 
 import { CitasComponent } from './citas/citas.component';
-const routes: Routes = [{path:"agenda",component:CitasComponent}];
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [{path:"agenda",component:CitasComponent,canActivate:[BatmanGuard]},
+{path:"**",component:LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
